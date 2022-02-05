4b53558399a3783941205cc8099f73f939dd003c
Dthariq347 <65454556+Dthariq347@users.noreply.github.com>
2022-02-05 19:28:40 +0700

    genarate laporan
    dapet men genarate per bulan, tahun dan dua dua nya dan semua nya

--- app/Controllers/Generate.php
+++ app/Controllers/Generate.php
@@ -38,6 +38,7 @@ class generate extends BaseController
         $bln_bayar = $this->request->getVar('bln_bayar');
         $thn_bayar = $this->request->getVar('thn_bayar');
         // walaupun tidak di input tetap print ALL
+
         if ($bln_bayar == NULL && $thn_bayar == NULL) {
 
             $data = [
@@ -50,6 +51,39 @@ class generate extends BaseController
                     ->join('kelas', 'kelas.id_kelas = siswa.id_kelas')
                     ->orderBy('bln_bayar', 'ASC')
                     ->orderBy('thn_bayar', 'ASC')
+                    ->paginate(50, 'pembayaran'),
+
+                'pager' => $this->PembayaranModel->pager,
+
+            ];
+
+
+            $html = view('/generate/read', $data);
+            $dompdf = new Dompdf();
+            $dompdf->loadHtml($html);
+            // (Optional) Setup the paper size and orientation
+            $dompdf->setPaper('A4', 'landscape');
+
+            // Render the HTML as PDF
+            $dompdf->render();
+
+            // Output the generated PDF to Browser
+
+            $dompdf->stream('invoice.pdf', array(
+                "Attachment" => false
+
+            ));
+        } elseif ($bln_bayar && $thn_bayar) {
+            $data = [
+                'title' => 'generate laporan (admin) | MTSN 3 Jakarta Selatan',
+                'read' => $this->PembayaranModel
+                    ->select('nis ,nama, nama_kelas, bln_bayar, thn_bayar, nominal, jumlah_bayar')
+                    ->join('users', 'users.id = pembayaran.id AND users.id = pembayaran.siswa', 'left')
+                    ->join('siswa', 'siswa.nisn = pembayaran.nisn')
+                    ->join('spp', 'spp.id_spp = pembayaran.id_spp')
+                    ->join('kelas', 'kelas.id_kelas = siswa.id_kelas')
+                    ->where('bln_bayar =', $bln_bayar)
+                    ->where('thn_bayar =', $thn_bayar)
                     ->paginate(50, 'pembayaran'),
 
                 'pager' => $this->PembayaranModel->pager,
@@ -138,40 +172,10 @@ class generate extends BaseController
                 "Attachment" => false
 
             ));
-        } else {
-            $data = [
-                'title' => 'generate laporan (admin) | MTSN 3 Jakarta Selatan',
-                'read' => $this->PembayaranModel
-                    ->select('nis ,nama, nama_kelas, bln_bayar, thn_bayar, nominal, jumlah_bayar')
-                    ->join('users', 'users.id = pembayaran.id AND users.id = pembayaran.siswa', 'left')
-                    ->join('siswa', 'siswa.nisn = pembayaran.nisn')
-                    ->join('spp', 'spp.id_spp = pembayaran.id_spp')
-                    ->join('kelas', 'kelas.id_kelas = siswa.id_kelas')
-                    ->where('bln_bayar =', $bln_bayar)
-                    ->where('thn_bayar =', $thn_bayar)
-                    ->paginate(50, 'pembayaran'),
-
-                'pager' => $this->PembayaranModel->pager,
-
-            ];
-
-
-            $html = view('/generate/read', $data);
-            $dompdf = new Dompdf();
-            $dompdf->loadHtml($html);
-            // (Optional) Setup the paper size and orientation
-            $dompdf->setPaper('A4', 'landscape');
-
-            // Render the HTML as PDF
-            $dompdf->render();
-
-            // Output the generated PDF to Browser
-
-            $dompdf->stream('invoice.pdf', array(
-                "Attachment" => false
-
-            ));
         }
+
+
+
 
 
         // untuk jalanin bulan bayar nya saja
